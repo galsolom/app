1746d72188f3f759258502e3ab1c5276ade1d7b9
mithrillx <galsolom@gmail.com>
2022-03-15 12:06:30 +0200

    py

---
+++ app/main.py
@@ -0,0 +1,22 @@
+
+from typing import Optional
+
+from fastapi import FastAPI, Response, status
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/health")
+def healthcheck():
+    return "Healthy"
+
+tasks = {"msg": "error"}
+
+@app.get("/error", status_code=500)
+def error_response(response: Response):
+    response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    return tasks["msg"]
