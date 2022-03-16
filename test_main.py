82d90e8369e3ce0d2c823dc711a3fe76fb88d839
mithrillx <galsolom@gmail.com>
2022-03-16 09:31:30 +0200

    another test

--- test_main.py
+++ test_main.py
@@ -6,7 +6,12 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_a():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
+
+def test_b():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
